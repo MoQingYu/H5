3ba6369241efdba47529ac48b38c4e13308488e6
liu xulei <liu.xulei@e-ports.com>
2018-09-21 16:22:25 +0800

    update

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -3,6 +3,7 @@ import { Frame } from "./frame";
 import createBrowserHistory from 'history/createBrowserHistory';
 import { Router, Route } from "react-router-dom";
 const hashHistory = createBrowserHistory();
+console.log(hashHistory);
 export const router = (
     <Router history={hashHistory}>
         <Route path="/" component={Frame}></Route>
